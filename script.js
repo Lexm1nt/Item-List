a095b7132a038b292f9d4b730eae238ea5a947b7
tyryshkin2002@yandex.ru <tyryshkin2002@yandex.ru>
2024-02-14 17:53:16 +0700

    Updated

--- script.js
+++ script.js
@@ -2,23 +2,30 @@ document.addEventListener("DOMContentLoaded", function () {
     var itemInput = document.querySelector(".itemInput");
     var addItemBtn = document.querySelector(".addItemBtn");
     var itemList = document.querySelector(".itemList");
+    /* Парсим элементы из localStorage */
     var items = JSON.parse(localStorage.getItem("items") || '""');
+    /*Если в localStorage есть массив элементов, выводим их */
     if (items) {
         items.forEach(function (item) {
             addItem(item);
         });
     }
+    /*Вешаем ивент на кнопку, если текст введен, обнуляем текст в инпуте и добавляем новый элемент в список, иначе меняем цвет границы инпута на красный */
     addItemBtn.addEventListener("click", function () {
         var newItemText = itemInput.value.trim();
         if (newItemText !== "") {
             addItem(newItemText);
             itemInput.value = "";
+            itemInput.placeholder = "Введите текст";
             itemInput.style.borderColor = "";
         }
         else {
             itemInput.style.borderColor = "red";
+            itemInput.placeholder = "Введите текст!";
         }
     });
+    /* Добавляем li, в него добавляем p с текстом из инпута, добавляем стили для стилизации, вешаем ивент на кнопку удаления элемента,
+    обновляем localStorage в случае добавления и удаления элемента */
     function addItem(text) {
         var li = document.createElement("li");
         li.className = "item";
@@ -28,8 +35,9 @@ document.addEventListener("DOMContentLoaded", function () {
         itemList.appendChild(li);
         li.appendChild(p);
         var removeBtn = document.createElement("button");
-        removeBtn.className = "item-button";
+        removeBtn.className = "delete-item-button";
         removeBtn.textContent = "✖";
+        removeBtn.setAttribute("aria-label", "Удалить элемент");
         removeBtn.addEventListener("click", function () {
             itemList.removeChild(li);
             updateLocalStorage();
@@ -37,6 +45,7 @@ document.addEventListener("DOMContentLoaded", function () {
         li.appendChild(removeBtn);
         updateLocalStorage();
     }
+    /* Обновляем localStorage, выделяем текст всех элементов, пушим их в массив, сохраняем */
     function updateLocalStorage() {
         var itemsEl = document.querySelectorAll("p");
         var items = [];
--- script.ts
+++ script.ts
@@ -1,8 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
+
   const itemInput = document.querySelector(".itemInput") as HTMLInputElement;
   const addItemBtn = document.querySelector(".addItemBtn") as HTMLButtonElement;
   const itemList = document.querySelector(".itemList") as HTMLUListElement;
+
+  /* Парсим элементы из localStorage */
+
   const items = JSON.parse(localStorage.getItem("items") || '""');
+
+
+
+  /*Если в localStorage есть массив элементов, выводим их */
 
   if (items) {
     items.forEach((item: string) => {
@@ -10,16 +18,24 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
+
+  /*Вешаем ивент на кнопку, если текст введен, обнуляем текст в инпуте и добавляем новый элемент в список, иначе меняем цвет границы инпута на красный */
+
   addItemBtn.addEventListener("click", function () {
     const newItemText = itemInput.value.trim();
     if (newItemText !== "") {
       addItem(newItemText);
       itemInput.value = "";
+      itemInput.placeholder = "Введите текст";
       itemInput.style.borderColor = "";
     } else {
       itemInput.style.borderColor = "red";
+      itemInput.placeholder = "Введите текст!";
     }
   });
+
+  /* Добавляем li, в него добавляем p с текстом из инпута, добавляем стили для стилизации, вешаем ивент на кнопку удаления элемента, 
+  обновляем localStorage в случае добавления и удаления элемента */
 
   function addItem(text: string) {
     const li = document.createElement("li");
@@ -29,9 +45,11 @@ document.addEventListener("DOMContentLoaded", function () {
     p.textContent = text;
     itemList.appendChild(li);
     li.appendChild(p);
+
     const removeBtn = document.createElement("button");
-    removeBtn.className = "item-button";
+    removeBtn.className = "delete-item-button";
     removeBtn.textContent = "✖";
+    removeBtn.setAttribute("aria-label", "Удалить элемент");
     removeBtn.addEventListener("click", function () {
       itemList.removeChild(li);
       updateLocalStorage();
@@ -40,6 +58,8 @@ document.addEventListener("DOMContentLoaded", function () {
     li.appendChild(removeBtn);
     updateLocalStorage();
   }
+
+  /* Обновляем localStorage, выделяем текст всех элементов, пушим их в массив, сохраняем */
 
   function updateLocalStorage() {
     const itemsEl = document.querySelectorAll("p");
